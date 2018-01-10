76d916bcb93b4ab9684efa91fa0fb21d0a2b4e18
Simon <Simon_Freelance@hotmail.com>
2018-01-10 09:46:39 -0500

    added working project of etch a sketch

--- main.js
+++ main.js
@@ -1,4 +1,48 @@
-"use strict"
-const resetBtn = document.getElementById("reset-btn");
-const newGridBtn = document.getElementById("new-grid-btn");
-const mainGrid = document.getElementById("grid-container");
+const newGridBtn = document.getElementById('new-grid-btn');
+const resetBtn = document.getElementById('reset-btn');
+const gridContainer = document.getElementById('grid-container');
+
+function createGridItem() {
+  let gridItem = document.createElement('div');
+  gridItem.classList.add('default-grid-item');
+  return gridItem;
+}
+
+function createTracks(numOfItems) {
+  let track = document.createElement('div');
+  track.classList.add('default-track');
+  for (let i = 0; i < numOfItems; i++) {
+    track.appendChild(createGridItem());
+  }
+  return track;
+}
+
+function createGrid(numOfItems) {
+  for (let i = 0; i < numOfItems; i++) {
+    gridContainer.appendChild(createTracks(numOfItems));
+  }
+  let gridItems = document.querySelectorAll('.default-grid-item');
+  for (let i = 0; i < gridItems.length; i++) {
+    gridItems[i].addEventListener('mouseover', function() {
+      gridItems[i].classList.add('light-up');
+    });
+  }
+  resetBtn.addEventListener('click', function() {
+    for (let i = 0; i < gridItems.length; i++) {
+      gridItems[i].classList.remove('light-up');
+    }
+  });
+}
+createGrid(16); //creates default 16 x 16 grid at the beginning
+
+newGridBtn.addEventListener('click', function() {
+
+  let howManyBlocks = prompt("Grid Size?");
+  howManyBlocks = parseInt(howManyBlocks);
+  //removes previous grid items
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  createGrid(howManyBlocks);
+  console.log(gridItems);
+});
